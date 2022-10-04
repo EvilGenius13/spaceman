7009174be96b042592e9ee5d664c695158ac3981
EvilGenius <93214149+EvilGenius13@users.noreply.github.com>
2022-10-03 20:16:20 -0400

    ascii test attempt

--- spaceman.py
+++ spaceman.py
@@ -1,5 +1,7 @@
 import random
 
+def ascii(wrong_answer):
+    ascii_dynamite = [ 'explode', '1 try', '2 try', '3 try', '4 try', '5 try', '6 try']
 
 ##                                          READY TO GO
 def load_word():
@@ -23,39 +25,43 @@ def load_word():
     """
 ##                                          READY TO GO
 def is_word_guessed(secret_word, letters_guessed):
-   
-  for character in secret_word:
-
-        if character not in letters_guessed:
-            return False
-        
-        else: 
-            return True
+    is_letter_guessed = True
+    
+    while is_letter_guessed:
+        for letter in secret_word:
+            if letter in letters_guessed:
+                is_letter_guessed = True
+            else:
+                return False
+        return True
 
 def get_guessed_word(secret_word, letters_guessed):
     guess=''
     
-    for i in range(len(secret_word)):
-        if secret_word[i] in letters_guessed:
-            guess += secret_word[i]
-        else: 
+    for letter in secret_word:
+        if letter in letters_guessed:
+            guess += letter
+        else:
             guess += '_'
+
     return guess
    
 ##                                          READY TO GO
 def is_guess_in_word(guess, secret_word):
     
     if guess in secret_word:
-        #print('Yay! You guessed right! Keep going.')
+        print("Yes Cadet! Keep Going!")
         return True
     else:
-        #print("Aww, that guess wasn't part of the word. Try again!")
+        print("Oh no! Try again!")
         return False 
+    
+    
     """
     Checks if a letter guessed is in the secret word
     ex: is the letter a in the secret word apple (yes)
 
-    paramaters:
+    parameters:
     guess : the letter input by user
     secret_word : the secret word
 
@@ -67,22 +73,22 @@ def is_guess_in_word(guess, secret_word):
 
 def spaceman(secret_word):
   
-    guesses = 7
-    letters_guessed = ['']
+    guesses_left = 7
+    letters_guessed = ''
 
     print('WHOOOOOOOOOOOOOOOSSSSSHHHHHHHHH')
-    print("Welcome to Spaceman. It's time to fly, or crash...")
+    print("Welcome to Spaceman. It's time to launch, or explode...")
     print("Your goal is to guess the secret word letter by letter. (Think knockoff wheel of fortune)")
     print("You have 7 guesses. Let's go to space!")
 
-    game_state = True
+    game_over = False
 
-    while guesses != 0:
+    while guesses_left != 0:
         userPrompt = input('Okay Cadet, take a guess: ')
         is_guess_in_word(userPrompt, secret_word)
         letters_guessed.append(userPrompt)
         get_guessed_word(secret_word, letters_guessed)
-        guesses -= 1
+        guesses_left -= 1
     if is_word_guessed():
         print('BLAST OFF!!!! CADET, YOU ARE PROMOTED!')
     else:
@@ -99,3 +105,7 @@ def spaceman(secret_word):
 #These function calls that will start the game
 secret_word = load_word()
 spaceman(secret_word)
+
+
+___________________   
+(________TNT_______|~~~~~~~X
