cba7adda06cad6fba6cebb89c1f0477597119609
EvilGenius <93214149+EvilGenius13@users.noreply.github.com>
2022-10-03 20:44:34 -0400

    re-write game-load and game_state

--- spaceman.py
+++ spaceman.py
@@ -1,7 +1,8 @@
 import random
 
 def ascii(wrong_answer):
-    ascii_dynamite = [ 'explode', '1 try', '2 try', '3 try', '4 try', '5 try', '6 try']
+    ascii_dynamite = [ '7', '6', '5', '4', '3', '2', '1', 'EXPLODE']
+    return wrong_answer
 
 ##                                          READY TO GO
 def load_word():
@@ -75,24 +76,37 @@ def spaceman(secret_word):
   
     guesses_left = 7
     letters_guessed = ''
-
+    wrong_answer = 0
+   
+    print("        |")
+    print('       / \ ')
+    print('      / _ \ ')
+    print("     |.o '.|")
+    print("     |'._.'|")
+    print("     |     |")
+    print("   ,'|  |  |`.")
+    print("   /  |  |  |  \ ")
+    print("   |,-'--|--'-.|  ") 
     print('WHOOOOOOOOOOOOOOOSSSSSHHHHHHHHH')
-    print("Welcome to Spaceman. It's time to launch, or explode...")
-    print("Your goal is to guess the secret word letter by letter. (Think knockoff wheel of fortune)")
-    print("You have 7 guesses. Let's go to space!")
-
-    game_over = False
+    print("Welcome to Spaceman. It's time to launch, or explode...") 
+    
+    
+    want_instructions = input("Do you want to learn how to play? y/n: ")
+    if want_instructions.upper == 'Y':
+        print("Your goal is to guess the secret word letter by letter. (Think knockoff wheel of fortune)")
+        print("You have 7 guesses. Let's go to space!")
+    else:
+        return
+   
 
     while guesses_left != 0:
-        userPrompt = input('Okay Cadet, take a guess: ')
-        is_guess_in_word(userPrompt, secret_word)
-        letters_guessed.append(userPrompt)
-        get_guessed_word(secret_word, letters_guessed)
-        guesses_left -= 1
-    if is_word_guessed():
-        print('BLAST OFF!!!! CADET, YOU ARE PROMOTED!')
-    else:
-        print('Sorry Cadet, better luck next time.')
+        print(f"You are down to {guesses_left}")
+        game_state = True
+
+        while game_state:
+
+            guess = input('Okay Cadet, take a guess: ')
+            
 
 
 
@@ -107,5 +121,5 @@ secret_word = load_word()
 spaceman(secret_word)
 
 
-___________________   
-(________TNT_______|~~~~~~~X
+# ___________________   
+# (________TNT_______|~~~~~~~X
